118acd9e52e62543f5cf1a5cf7891d4abf4dac75
rafael lima <ragpl07@gmail.com>
2021-04-25 20:57:42 -0300

    ok

--- androidutils/src/main/java/com/tomatedigital/androidutils/AndroidUtils.java
+++ androidutils/src/main/java/com/tomatedigital/androidutils/AndroidUtils.java
@@ -34,6 +34,8 @@ import androidx.annotation.WorkerThread;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -43,6 +45,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -461,5 +464,47 @@ public class AndroidUtils {
                 ((i >> 8) & 0xFF) + "." +
                 (i & 0xFF);
     }
+
+    public static   int getProcessorCoresCount() {
+        if(Build.VERSION.SDK_INT >= 17) {
+            return Runtime.getRuntime().availableProcessors();
+        }
+        else {
+            // Use saurabh64's answer
+            return getNumCoresOldPhones();
+        }
+    }
+
+    /**
+     * Gets the number of cores available in this device, across all processors.
+     * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+     * @return The number of cores, or 1 if failed to get result
+     */
+    private static int getNumCoresOldPhones() {
+        //Private Class to display only CPU devices in the directory listing
+        class CpuFilter implements FileFilter {
+            @Override
+            public boolean accept(File pathname) {
+                //Check if filename is "cpu", followed by a single digit number
+                if(Pattern  .matches("cpu[0-9]+", pathname.getName())) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        try {
+            //Get directory containing CPU info
+            File dir = new File("/sys/devices/system/cpu/");
+            //Filter to only list the devices we care about
+            File[] files = dir.listFiles(new CpuFilter());
+            //Return the number of cores (virtual CPU devices)
+            return files.length;
+        } catch(Exception e) {
+            //Default to return 1 core
+            return 1;
+        }
+    }
+
 
 }
---
+++ androidutils/src/main/java/com/tomatedigital/androidutils/ThreadPool.java
@@ -0,0 +1,76 @@
+package com.tomatedigital.androidutils;
+
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+
+
+import com.tomatedigital.utils.general.ThreadPoolExpandableExecutor;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadPool {
+
+
+    /**
+     * Creates a thread pool that creates new threads as needed, but
+     * will reuse previously constructed threads when they are
+     * available.  These pools will typically improve the performance
+     * of programs that execute many short-lived asynchronous tasks.
+     * Calls to {@code execute} will reuse previously constructed
+     * threads if available. If no existing thread is available, a new
+     * thread will be created and added to the pool. Threads that have
+     * not been used for sixty seconds are terminated and removed from
+     * the cache. Thus, a pool that remains idle for long enough will
+     * not consume any resources. Note that pools with similar
+     * properties but different details (for example, timeout parameters)
+     * may be created using {@link ThreadPoolExecutor} constructors.
+     *
+     * @return the newly created thread pool
+     */
+    private static ThreadPoolExecutor pool;
+
+    public static void startPool(int min, int max, int secs, @NonNull String prefix) {
+        pool = new ThreadPoolExpandableExecutor(min, max, secs, TimeUnit.SECONDS, new ThreadFactory() {
+            private final ThreadGroup group;
+            private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+            {
+                SecurityManager s = System.getSecurityManager();
+                group = (s != null) ? s.getThreadGroup() :
+                        Thread.currentThread().getThreadGroup();
+            }
+
+            @Override
+            public Thread newThread(@NotNull Runnable r) {
+                Thread t = new Thread(group, r,
+                        prefix + "-" + threadNumber.getAndIncrement(),
+                        0);
+                if (t.isDaemon())
+                    t.setDaemon(false);
+                if (t.getPriority() != Thread.NORM_PRIORITY)
+                    t.setPriority(Thread.NORM_PRIORITY);
+                return t;
+            }
+        });
+
+    }
+
+
+    public static void run(@NonNull Runnable r) {
+        ThreadPool.pool.execute(r);
+    }
+
+    public static Executor getExecutor() {
+        return pool;
+    }
+
+}
